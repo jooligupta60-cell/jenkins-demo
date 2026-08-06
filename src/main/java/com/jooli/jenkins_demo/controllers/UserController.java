9f4c0c791cc1dffb79c04969dd4d9073d17f3322
Jooli Gupta <jooligupta60@gmail.com>
2026-08-06 17:09:52 +0530

    fix controller

--- src/main/java/com/jooli/jenkins_demo/controllers/UserController.java
+++ src/main/java/com/jooli/jenkins_demo/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.jooli.jenkins_demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserController {
     @GetMapping("/home")
     public String Home(){
